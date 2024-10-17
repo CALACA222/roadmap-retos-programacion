be8bc558da60eef7c3b01adb4f2b40e8cf9c66ce
CALACA222 <brandon2001huesos@gmail.com>
2024-10-17 10:16:50 -0500

    Create CALACA222.js
    mi aportación al reto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/CALACA222.js
@@ -0,0 +1,24 @@
+/*https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// esto es comentario en una liena //
+
+/** estos son cometarios 
+en varias lineas 
+asi es**/
+
+var variable = "es mi variable";
+var variable = "nuevo valor de la variable"
+
+
+const constante = "Es mi contaste";
+
+var int = 22;
+var float = 1.9;
+var bool = true; 
+var bool = false;
+var string = "Mi cadena de texto";
+var other_string = 'Mi otra cadena de texto'
+
+
+
+console.log("hola, javaScript");
